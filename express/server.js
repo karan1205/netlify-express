b6ffcc8136c1053e4d6eef3444e66d5385b4542a
Karan Singh <karan1205singh@gmail.com>
2021-05-18 01:32:49 +0530

    mail service

--- express/server.js
+++ express/server.js
@@ -7,6 +7,8 @@ const axios = require('axios');
 const app = express();
 const parser = new Parser();
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -39,6 +41,36 @@ router.get('/travel', async (req, res) => {
   const {data} = files;
   const result = data.map(({name}) => (`https://ik.imagekit.io/batman/${name}`));
   return res.status(200).send({data: result});
+});
+
+router.get('/send-mail', async (req, res) => {
+  const {name, email, message} = req.query;
+  const transporter = nodemailer.createTransport(smtpTransport({
+    service: process.env.MAIL_SERVICE,
+    host: process.env.MAIL_HOST,
+    auth: {
+      user: process.env.MAIL_USER_EMAIL,
+      pass: process.env.MAIL_USER_PASS
+    }
+  }));
+  const text = `<p>Name: ${name}</p>
+               <p>Email: ${email}</p>
+               <p>Message: ${message}</p>`;
+  const mailOptions = {
+    from: process.env.MAIL_SERVICE,
+    to: email,
+    subject: 'Message from karansinghvirdi.com',
+    text
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+      return res.status(400).send({message: 'Something went wrong'});
+    } else {
+      return res.status(200).send({data: 'Message Sent Successfully'});
+    }
+  });
 });
 
 app.use(bodyParser.json());
